063b5b6cfbbbf40e6d58a9cfc79d7a429c37f54c
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-30 17:53:27 -0300

    se creo el componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Footer from "./components/Footer";
+
+
 
 function App() {
-  
-
   return (
     <>
-      
+    <h1 className="display-2 text-center py-2 text-bg-dark">Alta de peliculas</h1>
+      <Container className="mainPage">
+        
+      </Container>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
