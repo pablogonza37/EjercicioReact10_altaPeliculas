a767c5edd8e5486b96b62b5c7b60a1a0f4303ddb
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-30 17:33:27 -0300

    se agregó el boton de guardar al formulario

--- src/components/FormularioAltaPeliculas.jsx
+++ src/components/FormularioAltaPeliculas.jsx
@@ -1,4 +1,5 @@
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const FormularioAltaPeliculas = () => {
   return (
@@ -10,7 +11,11 @@ const FormularioAltaPeliculas = () => {
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Descripcion:</Form.Label>
-          <Form.Control as="textarea" rows={3} placeholder="Agregue una descripcion" />
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Agregue una descripcion"
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
           <Form.Label>Genero:</Form.Label>
@@ -21,6 +26,11 @@ const FormularioAltaPeliculas = () => {
             <option value="3">Three</option>
           </Form.Select>
         </Form.Group>
+        <div className="text-center mt-5">
+          <Button variant="success" className="w-50" type="submit">
+            Guardar
+          </Button>
+        </div>
       </Form>
     </section>
   );
