57a5a4046e68311f71b29506560d5354f314736f
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-30 17:27:49 -0300

    se creo´el formulario de alta de peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Footer from "./components/Footer";
-
+import FormularioAltaPeliculas from "./components/FormularioAltaPeliculas";
 
 
 function App() {
   return (
     <>
-    <h1 className="display-2 text-center py-2 text-bg-dark">Alta de peliculas</h1>
+      <h1 className="display-2 text-center py-2 text-bg-dark">
+        Alta de peliculas
+      </h1>
       <Container className="mainPage">
-        
+      <FormularioAltaPeliculas></FormularioAltaPeliculas>
       </Container>
       <Footer></Footer>
     </>
---
+++ src/components/FormularioAltaPeliculas.jsx
@@ -0,0 +1,29 @@
+import Form from "react-bootstrap/Form";
+
+const FormularioAltaPeliculas = () => {
+  return (
+    <section>
+      <Form className="text-white p-4 form my-4">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Nombre:</Form.Label>
+          <Form.Control type="email" placeholder="Nombre de peliculo" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Descripcion:</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="Agregue una descripcion" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+          <Form.Label>Genero:</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>Selecione el genero</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Select>
+        </Form.Group>
+      </Form>
+    </section>
+  );
+};
+
+export default FormularioAltaPeliculas;
